155e329baee9d72a194fb5b23c8a299378a60a20
spuds <spuds@spudsdesign.com>
2017-02-08 21:28:17 -0600

    ! add french language translation

---
+++ languages/air_elk.french.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+$txt['attachment_image_width'] = 'Largeur maximale autorisée des images attachées.';
+$txt['attachment_image_height'] = 'Hauteur maximale autorisée des images attachées.';
+$txt['attachment_image_reformat'] = 'Permet de reformater les images non JPEG au format JPEG lors du redimensionnement.';
+$txt['attachment_image_sub'] = '(0 pour aucune limite)';
+$txt['attachment_image_post'] = '(Les images plus grandes seront redimensionnées à cette limite)';
+$txt['attachment_image_resize'] = 'Redimensionnement d\'image de pièce jointe';
+$txt['attachment_image_enabled'] = 'Activer le redimensionnement d\'image de pièce jointe';
+$txt['attachment_image_SizeLimit'] = 'Taille maximale par pièce jointe';
+
+$txt['helpattachment_image_enabled'] = 'Interrupteur principal de marche / arrêt pour la fonction.';
+$txt['helpattachment_image_enabled_size'] = 'Ou supérieur à %s KB.';
+$txt['help_attachment_image_reformat'] = 'La sélection de cette option permettra au système de reformater les images non JPEG au format JPEG si nécessaire. Le système essaiera de maintenir le format existant à moins que l\'image résultante ne dépasse toujours la taille maximale autorisée.';
+$txt['help_attachment_image_width'] = 'Cela vous permet de définir une largeur maximale pour les images de pièces jointes. Les images inférieures au maximum ne seront pas affectées, les images plus grandes seront redimensionnées proportionnellement. Cela vous permet d\'accepter une image plus grande lors du téléchargement et de la faire redimensionner pour économiser de l\'espace. Le paramètre maximum de taille de fichier est toujours appliqué.';
+$txt['help_attachment_image_height'] = 'Cela vous permet de définir une hauteur maximale pour les images de pièces jointes. Les images inférieures au maximum ne seront pas affectées, les images plus grandes seront redimensionnées proportionnellement. Cela vous permet d\'accepter une image plus grande lors du téléchargement et de la faire redimensionner pour économiser de l\'espace. Le paramètre maximum de taille de fichier est toujours appliqué.';
