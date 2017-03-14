4296651353fc6cd90e58f709f500c821e96cdba5
spuds <spuds@spudsdesign.com>
2017-03-14 09:08:47 -0500

    ! fix language typos

--- languages/air_elk.english.php
+++ languages/air_elk.english.php
@@ -10,7 +10,7 @@ $txt['attachment_image_enabled'] = 'Enable Attachment Image Resizing';
 $txt['attachment_image_SizeLimit'] = 'Max size per attachment';
 
 $txt['helpattachment_image_enabled'] = 'Master on/off switch for the function.';
-$txt['helpattachment_image_enabled_size'] = 'or large than %s KB.';
+$txt['helpattachment_image_enabled_size'] = 'or larger than %s KB.';
 $txt['help_attachment_image_reformat'] = 'Selecting this option will allow the system to reformat non-JPEG images into the JPEG format when necessary.  The system will try to maintain the existing format unless the resulting image is still in excess of the maximum file size allowed.';
 $txt['help_attachment_image_width'] = 'This allows you to set a maximum width for attachment images. Pictures smaller than the maximum will not be affected, larger images will be resized proportionately. This allows you to accept a larger image on upload and have it resized to save space. The maximum filesize parameter is still enforced.';
-$txt['help_attachment_image_height'] = 'This allows you to set a maximum width for attachment images. Pictures smaller than the maximum will not be affected, larger images will be resized proportionately. This allows you to accept a larger image on upload and have it resized to save space. The maximum filesize parameter is still enforced.';
+$txt['help_attachment_image_height'] = 'This allows you to set a maximum height for attachment images. Pictures smaller than the maximum will not be affected, larger images will be resized proportionately. This allows you to accept a larger image on upload and have it resized to save space. The maximum filesize parameter is still enforced.';
