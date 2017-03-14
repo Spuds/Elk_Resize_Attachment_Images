9747492b223518d8ef8534a6e48bb7172897a9c9
spuds <spuds@spudsdesign.com>
2017-03-14 09:09:30 -0500

    ! add german translation

---
+++ languages/air_elk.german.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+$txt['attachment_image_width'] = 'Maximale Breite angehängter Bilder.';
+$txt['attachment_image_height'] = 'Maximale Höhe angehängter Bilder.';
+$txt['attachment_image_reformat'] = 'Bilder in einem anderen Format als JPEG dürfen bei einer Größenänderung zum JPEG-Format umformatiert werden.';
+$txt['attachment_image_sub'] = '(0 = keine Größenbeschränkung)';
+$txt['attachment_image_post'] = '(Größere Bilder werden auf diese Größe geändert)';
+$txt['attachment_image_resize'] = 'Größenänderung für Bild-Anhänge';
+$txt['attachment_image_enabled'] = 'Größenänderung für Bild-Anhänge aktivieren';
+$txt['attachment_image_SizeLimit'] = 'Maximale Größe pro Bild-Anhang';
+
+$txt['helpattachment_image_enabled'] = 'Komplette Aktivierung/Deaktivierung dieser Funktion.';
+$txt['helpattachment_image_enabled_size'] = 'oder größer als %s KB.';
+$txt['help_attachment_image_reformat'] = 'Das Auswählen dieser Option erlaubt dem System, das Bilder in einem anderen Format als JPEG zum JPEG-Format umformatiert werden, sofern notwendig. Das System wird versuchen, das vorhandene Format beizubehalten, sofern das daraus resultierende Bild nicht die maximal erlaubte Dateigröße überschreitet.';
+$txt['help_attachment_image_width'] = 'Diese Einstellung erlaubt das Setzen einer maximalen Breite für angehängte Bilder. Sind die Bilder kleiner als das Maximum, bleiben diese unverändert. Größere Bilder werden proportional angepasst. Dies erlaubt das Hochladen eines größeren Bildes, welches in der Größe angepasst wird, um Speicherplatz zu sparen. Die Einstellung der maximalen Dateigröße wird weiterhin erzwungen.';
+$txt['help_attachment_image_height'] = 'Diese Einstellung erlaubt das Setzen einer maximalen Höhe für angehängte Bilder. Sind die Bilder kleiner als das Maximum, bleiben diese unverändert. Größere Bilder werden proportional angepasst. Dies erlaubt das Hochladen eines größeren Bildes, welches in der Größe angepasst wird, um Speicherplatz zu sparen. Die Einstellung der maximalen Dateigröße wird weiterhin erzwungen.';
